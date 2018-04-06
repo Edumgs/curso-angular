c3b2325942eb092258a5bdb141eec621ecbd93a0
Eduardo Monges <emonges24@gmail.com>
2018-04-05 22:18:58 -0400

    Hasta la clase 5

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -12,19 +12,32 @@ export class EmpleadoComponent implements OnInit {
   
   public empleado: Empleado;
   public trabajadores: Array<Empleado>;
+  public trabajador_externo: boolean;
+  public color: string;
+  public color_seleccionado: string;
 
   constructor() {
-    this.empleado = new Empleado('Eduardo', 25, 'Programador', true);
+    this.empleado = new Empleado('Eduardo Monges', 25, 'Programador', true);
     this.trabajadores = [
       new Empleado('Manolo Martinez', 35, 'Administrativo', true),
-      new Empleado('Ana Lopez', 26, 'Analista', true),
+      new Empleado('Ana Lopez', 26, 'Analista', false),
       new Empleado('Juan Perez', 30, 'Cocinero', true)
     ];
+    this.trabajador_externo = false;
+    //this.color = 'red';
+    //this.color = 'blue';
+    //this.color = 'green';
+    this.color = 'teal';
+    this.color_seleccionado = '#ccc';
   }
 
   ngOnInit() { 
     console.log(this.empleado);
     console.log(this.trabajadores);
+  }
+
+  cambiarExterno(valor) {
+    this.trabajador_externo = valor;
   }
 
 }
